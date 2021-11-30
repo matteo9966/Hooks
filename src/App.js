b5f69903ca74a0225bacd0a5d992bf8290582df2
Matteo Luigi Tommasi <75813215+matteo9966@users.noreply.github.com>
2021-11-30 21:01:27 +0100

    Aggiunto un nuovo hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import "./styles.css";
-import React from "react";
 // import { TimeoutComponent } from "./hooks/useTimeout/TimeoutComponent";
-// import { DebounceComponent } from "./hooks/useDebounce/DebounceComponent";
-import { UpdateEffectComponent } from "./hooks/useUpdateEffect/UpdateEffectComponent";
-export default function App() {
-  // const setState = React.useState()[1];
+import { DebounceComponent } from "./hooks/useDebounce/DebounceComponent";
+import { StateWithHistoryComponent } from "./hooks/useStateWithHistory/StateWithHistoryComponent";
 
+export default function App() {
   return (
     <div className="App">
       <h1>Custom Hooks!!</h1>
-      {/* <button onClick={setState}>setstate</button> */}
       {/* <TimeoutComponent /> */}
       {/* <DebounceComponent /> */}
-      <UpdateEffectComponent />
-      {console.log("rendering---")}
+      <StateWithHistoryComponent />
     </div>
   );
 }
